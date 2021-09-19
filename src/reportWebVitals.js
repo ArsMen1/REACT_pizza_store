230642e91b4d2a806513a4fc2985a98073e51bf3
Osip <arsenij-osipov@mail.ru>
2021-09-19 15:46:39 +0300

    Закоментил

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,11 +1,11 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry); // - измеряет степень стабильности контента на вашем сайте
+      getFID(onPerfEntry); // - измеряет время отзывчивости вашего сайта. Чем этот показатель ниже, тем больше у посетителей уверенности, что с вашим сайтом можно взаимодействовать
+      getFCP(onPerfEntry); // -
+      getLCP(onPerfEntry); // - измеряет время отрисовки самой большой и видимой части содержимого на первом экране вашего сайта
+      getTTFB(onPerfEntry); // -
     });
   }
 };
