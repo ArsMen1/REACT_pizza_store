7ada27534c866e68e30e53daa50fc2734393a3de
Osip <arsenij-osipov@mail.ru>
2021-10-10 23:10:00 +0300

    PizzaBlock++

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import axios from "axios";
+import PropTypes from "prop-types";
 import React from "react";
 import { Route } from "react-router";
-import { Header } from "./components";
+import { Header, PizzaBlock } from "./components";
 import { Home, Cart } from "./pages";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
   );
 }
+
+PizzaBlock.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default App;
